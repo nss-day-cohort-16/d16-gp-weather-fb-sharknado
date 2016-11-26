e1c92cd3bbdbc96a23fd7c78edd65d474d1ad059
Abby Moore <abbyleighanne@gmail.com>
2016-11-25 19:29:28 -0600

    output api to dom

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,33 @@
 "use strict";
+
+console.log("app.js"); 
+
+
+var zipCode = document.getElementById("zipCode");
+var submitBtn = document.getElementById("submitZip");
+
+
+function getWeather(zipcode){
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&APPID={ADD KEY HERE}`,
+			dataType: "json"
+		}).done(function(data) {
+			resolve(data);
+		}).fail((error) => {
+			reject(error);
+		});
+	});
+}
+
+
+function readData(data) {
+	console.log(data);
+}
+
+
+getWeather(37091)
+	.then((data) =>{
+		readData(data);
+		buildWeather(data);
+	});
--- javascripts/dom-handler.js
+++ javascripts/dom-handler.js
@@ -1,1 +1,17 @@
 "use strict";
+
+console.log("dom-handler.js");
+
+//pushes info to dom
+function buildWeather(data){
+	var output = document.getElementById("single-day-view");
+	var convertTemp = ((data.main.temp - 273.15) * 9/5) + 32;
+
+	output.innerHTML = 
+	`Current city: ${data.name}.<br>
+	 The temperature is ${convertTemp.toFixed(0)}&deg; F. <br>
+	 The conditions are ${data.weather[0].main}.<br>
+	 The wind speed is ${data.wind.speed} mph. <br>
+	 The air pressure is ${data.main.pressure}.
+	 `;
+}
