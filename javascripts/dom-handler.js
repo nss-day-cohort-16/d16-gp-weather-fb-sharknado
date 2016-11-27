5efd6a7f0c1c7385e171d062b629bf73a0b77d7d
Shawn M <forgeforza@outlook.com>
2016-11-27 00:03:51 -0600

    userinput zipcode 50percent

--- javascripts/dom-handler.js
+++ javascripts/dom-handler.js
@@ -1,1 +1,18 @@
 "use strict";
+
+/*
+var submit = document.getElementById("button");
+var field = document.getElementById("input").value;
+var zipcode = new RegExp(/[0-9][^a-z]{5}/).field;
+
+submit.addEventListener("click", getUserput);
+
+function getUserput(event) {
+    if (event.keyCode === 13 && ){
+        alert("zipcode is valid");
+    } else  {
+        alert("zipcode is invalid");
+    } 
+};
+
+
